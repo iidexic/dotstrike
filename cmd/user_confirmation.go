52eb51ee97838af5fcfe7a6082eddfbccb872f99
iidexic <barrierloss@yahoo.com>
2025-07-01 01:02:12 -0700

    feat:user confirmation request
    Added user confirm (y/n) to request confirmation on potentially
    uncertain or unexpected changes to data.

---
+++ cmd/user_confirmation.go
@@ -0,0 +1,52 @@
+package cmd
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func askConfirmf(detail string, vars ...any) bool {
+	reader := bufio.NewReader(os.Stdin)
+	n := 4
+	fdetail := "[request to confirm]: " + detail + "\n:::"
+	var noindex, yesindex int
+	for {
+		fmt.Printf(fdetail, vars...)
+		response, err := reader.ReadString('\n')
+		if err != nil {
+			panic(err)
+		}
+		response = strings.ToLower(response)
+		for _, no := range []string{"n", "no", "false"} {
+			if hasno := strings.Contains(response, no); hasno {
+				noindex = strings.Index(response, no)
+				break
+			}
+		}
+		for _, yes := range []string{"y", "yes", "true"} {
+			if hasYes := strings.Contains(response, yes); hasYes {
+				yesindex = strings.Index(response, yes)
+				break
+			}
+		}
+		if yesindex >= 0 && noindex >= 0 {
+			if yesindex < noindex {
+				return true
+			} else {
+				return false
+			}
+
+		} else if yesindex >= 0 {
+			return true
+		} else if noindex >= 0 {
+			return false
+		}
+		n--
+		if n < 0 {
+			return false
+		}
+
+	}
+}
