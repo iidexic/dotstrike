980e792a126ad1fd854324a36b8e16efb0cb6a0f
iidexic <barrierloss@yahoo.com>
2025-07-08 00:07:49 -0700

    implemented first pass of dir copy functionality
    untested. structs and functions for dir copy in place.

--- pathops/moveops.go
+++ pathops/moveops.go
@@ -1,64 +1,225 @@
 package pops
 
 import (
+	"fmt"
+	"io"
 	"io/fs"
 	"path/filepath"
+	"strings"
+)
+
+type esource = byte
+
+const (
+	_ esource = iota
+	esInfileOPEN
+	esOutfileMAKEOPEN
+	esCOPY
+	es
 )
 
 type DirEntry = fs.DirEntry
 
-// CopierMaschine builds and executes CopyJobs
-type CopierMaschine struct {
-	JobQueue []CopyJob
+// copierMaschine builds and executes CopyJobs
+// Ideally single-instance; use GetCopier to get
+type copierMaschine struct {
+	JobQueue map[string]*CopyJob
+}
+
+// primary-instance use
+var cmachine copierMaschine
+
+func GetCopierMaschine() *copierMaschine { return &cmachine }
+
+// NewJob creates a new job and adds to the JobQueue; returns true if successful
+// jobName must be unique within the JobQueue; if NewJob is passed an existing jobName,
+// it will not add the job to the queue, and will return false
+func (CM *copierMaschine) NewJob(jobName, pathIn, pathOut string) *CopyJob {
+	if CM.jobExists(jobName) {
+		return nil
+	}
+	CM.JobQueue[jobName] = &CopyJob{PathIn: pathIn, PathOut: pathOut}
+	return CM.JobQueue[jobName]
+}
+
+func (CM *copierMaschine) jobExists(jobName string) bool {
+	for k := range CM.JobQueue {
+		if k == jobName {
+			return true
+		}
+	}
+	return false
+}
+
+// GetJob returns *CopyJob if jobName exists in the JobQueue
+// otherwise returns nil ptr
+func (CM *copierMaschine) GetJob(jobName string) *CopyJob {
+	for keyName, ptrjob := range CM.JobQueue {
+		if keyName == jobName {
+			return ptrjob
+		}
+	}
+	return nil
+}
+
+// RunJob is equivalent to running GetJob and then running CopyJob.Run(nil)
+func (CM *copierMaschine) RunJob(jobName string) *CopyJob {
+	if ptrjob := CM.GetJob(jobName); ptrjob != nil {
+		ptrjob.Run()
+		return ptrjob
+	}
+	return nil
 }
 
 // CopyJob prepares and executes the copy of all contents of PathIn to PathOut
+// Pre-Run:
+// PathIn & PathOut are root dir of original files and copy location, respectively
+// ignore contains list of files to ignore; must be populated before running
+// JobSettings contains job-specific settings that will affect how copy job runs
+// Post-Run:
+// fstack will be populated when the job is run, providing a record of what is copied
+// OpErrors contains encountered errors, using fs.PathError format.
 type CopyJob struct {
 	PathIn, PathOut string
+	originalPathOut string // will be populated if makeRootSubdir = true
 	fstack          []filecopy
-	fsCopied        []float32
 	ignore          IgnoreSet
+	OpErrors        []fs.PathError
+	JobSettings     copyConfig
 }
 
-// filecopy will run a single file copy task and acts as a record of that task
-// TODO:re-tool; Originally did not plan to perform all copying in a single WalkDir
+type copyConfig struct {
+	makeRootSubdir bool
+	//DRY_RUN bool
+}
+
+// SetOptionMakeSubdir - sets CopyJob.JobSettings.makeRootSubdir
+// this appends the PathIn dir name to PathOut
+// if true: given PathIn == (base-pathIn...\DirName), set PathOut = (PathOut\DirName)
+// if false: Copy directly to provided PathOut
+func (J *CopyJob) SetOptionMakeSubdir(makeSubdir bool) {
+	J.JobSettings.makeRootSubdir = makeSubdir
+}
+
+// filecopy acts as a record of a single file's copy operation
 type filecopy struct {
-	name            string
+	name, relpath   string
 	id              int //?
 	inPath, outPath string
-	inSize, outSize int
+	inSize, outSize int64
 }
+
+// Removed for now.
+// type elog struct {
+// 	fs.PathError
+// 	relpath string
+// 	esrc    esource
+// }
 
 func (J *CopyJob) Load(absPathIn, absPathOut string) {
 	J.fstack = append(J.fstack, filecopy{
 		inPath: absPathIn, outPath: absPathOut,
 	})
 }
-
-func (J *CopyJob) newFcopy(in, out string) *filecopy {
-	return &filecopy{inPath: in, outPath: out}
-}
-
-func (J *CopyJob) Walk(p string, d DirEntry, e error) error {
-	if d.IsDir() {
-		if J.ignore.IsIgnoredDir(p) {
-			return fs.SkipDir
-		}
-		return nil
+func (J *CopyJob) Run( /* params */ ) error {
+	if J.JobSettings.makeRootSubdir {
+		J.originalPathOut = J.PathOut
+		J.PathOut = filepath.Join(J.PathOut, filepath.Base(J.PathIn))
 	}
-
-	return nil
-}
-
-func (J *CopyJob) RunCopyWalk() error {
 	e := filepath.WalkDir(J.PathIn, J.Walk)
 	if e != nil {
+		J.OpErrors = append(J.OpErrors, fs.PathError{Path: J.PathIn, Err: e, Op: ""})
 		return e
 	}
 	return nil
 }
 
+func (J *CopyJob) logError(abspath, opname string, e error) {
+	J.OpErrors = append(J.OpErrors, fs.PathError{Path: abspath, Op: opname, Err: e})
+}
+
+func (J *CopyJob) checkAndLogError(abspath, opname string, e error) {
+	if e != nil {
+		J.logError(abspath, opname, e)
+	}
+}
+
+// NOTE: Removed. Amount of detail unnecessary
+func (J *CopyJob) newFilecopy(inInfo *fs.FileInfo, inPath, outPath string, outSize int64) *filecopy {
+	J.fstack = append(J.fstack, filecopy{name: (*inInfo).Name(),
+		inPath: inPath, outPath: outPath, outSize: outSize})
+	return &J.fstack[len(J.fstack)-1]
+}
+
+func (J *CopyJob) addFile(relpath string, inSize, outSize int64) {
+	J.fstack = append(J.fstack, filecopy{relpath: relpath, inSize: inSize, outSize: outSize})
+}
+
+func (J *CopyJob) Walk(p string, d DirEntry, e error) error {
+	// DIRECTORIES:
+	if d.IsDir() {
+		if J.ignore.isIgnored(p) {
+			return fs.SkipDir
+		}
+		return nil
+	}
+	// FILES:
+	// ── 0. make filepath out ─────────────────────────────────
+	prel := J.stripRoot(p) //	!INFO: panics on error; error is unexpected(expand)
+	prr := prel
+	// add folder name if option enabled
+	if J.JobSettings.makeRootSubdir {
+		prr = filepath.Join(filepath.Base(J.PathIn), prr)
+	}
+	pto := filepath.Join(J.PathOut, prr)
+	if !filepath.IsAbs(pto) {
+		pto = absNoE(pto) //	!INFO: panics on error; error is unexpected(expand)
+	}
+	// ── 1. open in file ──────────────────────────────────────
+	inF, e := OpenExistingFile(p)
+	defer inF.Close()
+	if e != nil {
+		J.logError(p, "OpenExisting_In", e)
+		return nil //skip file if opening errors
+	}
+	// ── 2. make+open out file ────────────────────────────────
+	outF, e := MakeOpenFileF(pto)
+	defer outF.Close()
+	J.checkAndLogError(pto, "MakeOpen_Out", e)
+	// ── 3. perform copy ──────────────────────────────────────
+	wb, e := io.CopyBuffer(outF, inF, nil)
+	J.checkAndLogError(pto, "CopyError_Out", e)
+
+	// check size original matches new copied
+	inDE, e := d.Info()
+	J.checkAndLogError(p, "GetFileInfo_In", e)
+	J.addFile(prel, inDE.Size(), wb)
+	return nil
+}
+
+// absNoE runs abs and returns the resulting string; panics on error
+func absNoE(p string) string {
+	po, e := filepath.Abs(p)
+	if e != nil {
+		panic(e)
+	}
+	return po
+}
+
+// stripRoot removes CopyJob.PathIn from arg path `rpath` for construction of destination path
+// structure/intent of CopyJob requires J.PathIn to be a prefix in rpath.
+// As such, if an error is encountered, stripRoot panics
+func (J *CopyJob) stripRoot(rpath string) string {
+	p, e := filepath.Rel(rpath, J.PathIn)
+	if e != nil {
+		panic(fmt.Errorf("stripRoot(%s) error: %v", rpath, e))
+	}
+	return p
+
+}
+
 // ── Ignore Functionality ────────────────────────────────────────────
+// TODO: Finish Ignore system. For now it's not priority
 // IgnoreSet stores & processes ignore data for a CopyJob
 type IgnoreSet struct {
 	ignoreDir  []iptn
@@ -75,11 +236,16 @@ type iptn struct {
 	psize      byte
 }
 
+// matches checks string against the valid iptn
 func (ip iptn) matches(s string) bool {
+	if strings.Index(s, ip.pattern) >= 0 {
+		return true
+	}
+
 	return false
 }
 
-func (I *IgnoreSet) IsIgnoredDir(dirpath string) bool {
+func (I *IgnoreSet) isIgnored(dirpath string) bool {
 	for _, pat := range I.ignoreDir {
 		if pat.matches(dirpath) {
 			return true
--- pathops/moveops_test.go
+++ pathops/moveops_test.go
@@ -1,142 +1,19 @@
 package pops
 
 import (
-	"fmt"
-	"io"
-	"os"
-	"path/filepath"
 	"testing"
 )
 
-func checkbasicpath(fpath string, dest string, t *testing.T) {
-	if !IsBasicPath(fpath) {
-		t.Logf("not basic path: '%s'", fpath)
-		t.Fail()
+func TestCopyDir(t *testing.T) {
+	srcDir := "D:/coding/exampleFiles"
+	outDir := "C:/dev/.test_data/file_operations"
+	//NOTE: Testing makeRootSubdir Code; normally just write it out. Path doesn't need to exist
+	cm := GetCopierMaschine()
+	cm.NewJob("test_examplefiles", srcDir, outDir)
+	tcopy := cm.GetJob("test_examplefiles")
+	err := tcopy.Run()
+	if err != nil {
+		t.Errorf("COPY ERROR: %v", err)
+		t.Logf("[COPYJOB: %+v]", tcopy)
 	}
-	if !IsBasicPath(dest) {
-		t.Logf("not basic path: '%s'", dest)
-		t.Fail()
-	}
-	fpdetail := DetailStatPath(fpath)
-	ddetail := DetailStatPath(dest)
-
-	t.Log(fpdetail)
-	t.Log(ddetail)
-
-}
-
-func findProjdir(upath, projname string, t *testing.T) string {
-	if !filepath.IsAbs(upath) {
-		if filepath.Ext(upath) == "" {
-			//prob dir
-		}
-
-		e := os.Chdir("..")
-		if e != nil {
-			t.Logf("Chdir error: %e", e)
-			e = nil
-		}
-		uabs, e := filepath.Abs(upath)
-		if e != nil {
-			t.Logf("Get abspath from '%s' failed: %e", upath, e)
-		} else {
-			upath = uabs
-		}
-	}
-	return upath
-}
-
-func testLilCopy(fpath string, dest string, t *testing.T) {
-
-	//checkbasicpath(fpath, dest, t)
-	filefrom, efrom := OpenExistingFile(fpath)
-	if efrom != nil {
-		t.Logf("error opening filefrom: %e", efrom)
-	}
-	if filefrom == nil {
-		t.Error("filefrom: no file data")
-	}
-	defer filefrom.Close()
-	dpath, e := filepath.Abs(dest)
-	if e != nil {
-		t.Error(e)
-	}
-
-	fileto, eto := MakeOpenFileF(dpath)
-	t.Logf("file-to Fd: %v", fileto.Fd())
-	t.Log(eto)
-	if eto != nil {
-		t.Log("Error MakeOpenFile: ")
-		t.Error(eto)
-	}
-	defer fileto.Close()
-	written, ecpy := io.Copy(fileto, filefrom)
-	if ecpy != nil {
-		t.Log("(first) Error Copying")
-		t.Error(ecpy)
-	}
-	t.Log(fmt.Sprintf("bytes copied: %d", written))
-
-}
-
-func TestLilCopyGetToml(t *testing.T) {
-	configtoml := HomeDirtyJoin(".config/dotstrike/dotstrikeData.toml")
-	loctoml := findProjdir("./_xtra/dotstrikeData.toml", "dotstrike", t)
-	testLilCopy(configtoml, loctoml, t)
-}
-
-func TestLilCopyPushToml(t *testing.T) {
-	testLilCopy("./_xtra/dotstrikeData.toml", "~/.config/dotstrike/dotstrikeData.toml", t)
-}
-
-func TestStatHomePath(t *testing.T) {
-	//paths := []string{"/~/", "\\~\\", `~\\`, `~~//`}
-	hdir, errh := HomeJoin(".config/")
-	if errh != nil {
-		t.Errorf("HomeDir Error %v", errh)
-	}
-	info, e := os.Stat(hdir)
-	infostr := fmt.Sprintf(`path:%s stat:%+v`, hdir, info)
-	if e != nil {
-		t.Logf("%s | error %v", infostr, e)
-	} else {
-		t.Log(infostr)
-		t.Logf("dir? -> %t", info.IsDir())
-	}
-}
-
-func TestLStatPaths(t *testing.T) {
-	paths := []string{".", "./", `./`, ".\\", `.\\`, "~", `~`,
-		"~/", `~/`, "~/.config", `~/.config`, "~//.config", `~//.config`,
-		"~/.config/dotstrike/dotstrikeData.toml", "~//.config//dotstrike//dotstrikeData.toml",
-		`~/.config/dotstrike/dotstrikeData.toml`, `c:\~\`, `d:\~\`}
-
-	for i, p := range paths {
-		info, e := os.Lstat(p)
-		infostr := fmt.Sprintf(`%d) path:%s stat:%+v`, i, p, info)
-		if e != nil {
-			t.Logf("%s | error %v", infostr, e)
-		} else {
-			t.Log(infostr)
-		}
-	}
-}
-func TestStatPaths(t *testing.T) {
-	paths := []string{".", "./", `./`, ".\\", `.\\`, "~", `~`,
-		"~/", `~/`, "~/.config", `~/.config`, "~//.config", `~//.config`,
-		"~/.config/dotstrike/dotstrikeData.toml", "~//.config//dotstrike//dotstrikeData.toml",
-		`~/.config/dotstrike/dotstrikeData.toml`, `c:\~\`, `d:\~\`}
-
-	for i, p := range paths {
-		info, e := os.Stat(p)
-		infostr := fmt.Sprintf(`%d) path:%s stat:%+v`, i, p, info)
-		if e != nil {
-			t.Logf("%s | error %v", infostr, e)
-		} else {
-			t.Log(infostr)
-		}
-	}
-}
-func TestPrintDebug(t *testing.T) {
-	t.Log()
 }
