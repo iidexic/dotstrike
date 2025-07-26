d9a7188af1500483261548df6a761311eda040fd
iidexic <barrierloss@yahoo.com>
2025-07-26 01:17:04 -0700

    refactor: spec command WIP re-write
    break out spec operation into more clear functions/flow (not yet
    implemented)

--- cmd/spec_cmd.go
+++ cmd/spec_cmd.go
@@ -12,7 +12,7 @@ import (
 )
 
 type specFlags struct {
-	modify, yconfirm *bool
+	modify, yconfirm bool
 }
 
 var flagDataSpec specFlags
@@ -36,7 +36,7 @@ var specCmd = &cobra.Command{
 			for _, astr := range args {
 				found := dscore.SelectSpec(astr)
 				// if not found and -y flag or user confirmation, make new cfg
-				if !found && (*flagDataSpec.yconfirm || askConfirmf("Create new Spec: %s", astr)) {
+				if !found && (flagDataSpec.yconfirm || askConfirmf("Create new Spec: %s", astr)) {
 					td := dscore.GetTempData()
 					if td == nil {
 						panic(fmt.Errorf("TempData nil"))
@@ -103,11 +103,69 @@ func (f specFlags) identify(pf persistentData) specOptype {
 	return 0
 }
 
+func (op *specOpData) populateExisting(args []string) []string {
+	notFound := make([]string, 0, len(args))
+	for i := range args {
+		_ = i
+		if spec := dscore.GetSpec(args[i]); spec != nil {
+			op.existingSpecs = append(op.existingSpecs, spec)
+			op.argExists[i] = true
+		} else {
+			notFound = append(notFound, args[i])
+		}
+	}
+	return notFound
+}
+func (op *specOpData) pprintExisting() {
+	for i, spec := range op.existingSpecs {
+		op.cmd.Print(i, ".   ", spec.Detail())
+
+	}
+}
+
+// haveExistingExecute called when have existingSpecs; determines and executes next process steps
+func (op *specOpData) haveExistingExecute() {
+	if len(op.args) == 1 || len(op.existingSpecs) == op.argcount {
+	}
+}
+
+func specRun(cmd *cobra.Command, args []string) {
+	// if not a spec: make new spec
+	// if spec exists, show info
+	opSpec := specOpData{
+		cmd: cmd, flags: flagDataSpec,
+		args: args, argcount: len(args),
+		argExists: make([]bool, len(args)),
+	}
+
+	notExists := opSpec.populateExisting(args) // may not be necessary to return notExists
+	if len(notExists) == len(args) && len(args) > 0 {
+		// good to make new ones
+	} else if len(opSpec.existingSpecs) > 0 {
+		// check what we have
+		opSpec.haveExistingExecute()
+	}
+	_ = notExists
+
+}
+func countModFlags() int {
+	nlocal := 0
+	// if flagDataSpec.modify{
+	// 	nlocal++
+	// }
+	return nlocal + pData.countFlags
+}
+
+func (f specOptype) identify(pf persistentData) {
+	//flags:
+	//persistent:
+	//pData
+}
 func init() {
 	rootCmd.AddCommand(specCmd)
 	flagDataSpec = specFlags{
-		modify:   specCmd.Flags().BoolP("modify component", "m", false, "modify"),
-		yconfirm: specCmd.Flags().BoolP("autoconfirm user y/n prompts", "y", false, "yes"),
+		modify:   *specCmd.Flags().BoolP("modify component", "m", false, "modify"),
+		yconfirm: *specCmd.Flags().BoolP("autoconfirm user y/n prompts", "y", false, "yes"),
 	}
 
 	// PERSISTENT FLAGS:
