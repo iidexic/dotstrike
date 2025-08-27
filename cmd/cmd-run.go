fb123f62ed03e5497203bd87ed2645db3e876cf8
iidexic <barrierloss@yahoo.com>
2025-08-27 12:04:54 -0700

    feat: all options as separate flags for run command

--- cmd/cmd-run.go
+++ cmd/cmd-run.go
@@ -12,35 +12,49 @@ import (
 )
 
 func init() {
+	// Use Overrides? Or make flags for all?
+	/*
+		fOverridesUsage := `Set one-time overrides with a space-separated list of 'prefName value' pairs.
+		check spec help for more details on available options.`
+	*/
+	fNoFileUsage := "Disable filecopy for run. Use for dry runs, or with --all-dir to copy only the directory structure"
+	fAllDirUsage := `Copy all Source subdirectories, including empty subdirectories. 
+Use with --no-files to only copy the directories themselves.`
+	fManualUsage := `--manual --src="srcpath,[additional paths]" --tgt="tgtpath,[additional paths]"
+Use to run a one-time copy job. REQUIRES use of src and tgt flags to input paths to copy from/to. 
+	Use override flag to set run configuration; by default current global prefs will be used. `
+	fPartialUsage := `--partial="s(1,n),t(2,n)"
+provides ability to copy a subset of a given spec's sources and targets. To use, include indices or dir names of sources and targets as shown`
 	rootCmd.AddCommand(runCmd)
 	mainRun.flagAll = runCmd.Flags().Bool("all-specs", false, "Run ALL spec copy jobs")
 	mainRun.flagNoSelectedSpec = runCmd.Flags().Bool("no-selected", false, "Disable run of selected spec")
 	mainRun.flagY = runCmd.Flags().BoolP("confirm", "y", false, "Auto-Confirm all prompts during run")
-	mainRun.flagOverrides = runCmd.Flags().StringArray("override", []string{},
-		`Set one-time overrides with a space-separated list of 'prefName value' pairs.
-check spec help for more details on available options.`)
-	mainRun.flagNoFiles = runCmd.Flags().BoolP("no-files", "n", false,
-		"Disable filecopy for run. Use for dry runs, or with --all-dir to copy only the directory structure")
-	mainRun.flagAllDir = runCmd.Flags().BoolP("all-dirs", "d", false,
-		`Copy all Source subdirectories, including empty subdirectories. 
-Use with --no-files to only copy the directories themselves.`)
-	mainRun.flagRunPartial = runCmd.Flags().StringArray("partial", []string{}, "partial")
-	mainRun.flagManualRun = runCmd.Flags().StringArray("manual", []string{},
-		"manual s=`c:\\data`,personalDocs t=d:\\backup")
-	mainRun.flagSources = runCmd.Flags().StringArray("src", []string{}, `--src="path1,path2" (use for partial/manual)`)
-	mainRun.flagTargets = runCmd.Flags().StringArray("tgt", []string{}, "partial ")
+	//mainRun.flagOverrides = runCmd.Flags().StringArray("override", []string{}, fOverridesUsage)
+	mainRun.fOptNoFiles = runCmd.Flags().BoolP("no-files", "n", false, fNoFileUsage)
+	mainRun.fOptAllDirs = runCmd.Flags().BoolP("all-dirs", "d", false, fAllDirUsage)
+	mainRun.fOptGlobalTarget = runCmd.Flags().Bool("globaltarget", false, "'--globaltarget=false' to disable write to global target")
+	mainRun.fOptNoRepo = runCmd.Flags().Bool("ignore-repo", false, "add git repo to global ignores; Disables copying .git dir/repo dir")
+	mainRun.fOptNoHidden = runCmd.Flags().Bool("ignore-hidden", false, "add hidden paths  to global ignores; Disables copy of paths that begin with `_` or `.`")
+	mainRun.flagRunPartial = runCmd.Flags().StringArray("partial", []string{}, fPartialUsage)
+	mainRun.fOptManualRun = runCmd.Flags().Bool("manual", false, fManualUsage)
+	mainRun.flagSources = runCmd.Flags().StringArray("src", []string{}, `--src="path1,path2" (for partial/manual)`)
+	mainRun.flagTargets = runCmd.Flags().StringArray("tgt", []string{}, `--tgt="path1,path2" (for partial/manual)`)
+
 }
 
 type runner struct {
 	*cobra.Command
-	specs                                        []*dscore.Spec
-	args                                         []string
-	flagY, flagNoSelectedSpec, flagAll           *bool
-	flagNoFiles, flagAllDir                      *bool
-	flagOverrides, flagRunPartial, flagManualRun *[]string
-	flagSources, flagTargets                     *[]string
-	bOverrides, bManual, bPartial                bool
-	specNames                                    []string
+	specs                              []*dscore.Spec
+	args                               []string
+	flagY, flagNoSelectedSpec, flagAll *bool
+
+	fOptNoFiles, fOptAllDirs, fOptRootSubdir, fOptManualRun *bool
+	fOptNoHidden, fOptNoRepo, fOptGlobalTarget              *bool
+
+	flagOverrides, flagRunPartial *[]string
+	flagSources, flagTargets      *[]string
+	bOverrides, bPartial          bool
+	specNames                     []string
 }
 
 var mainRun runner
@@ -60,6 +74,9 @@ Modify a run with one-time overrides, perform a partial run, or run a one time m
 }
 
 func (r *runner) run(cmd *cobra.Command, args []string) {
+	// 1. check flags
+	// 1a:
+	// 1-2. make spec list
 	e := r.calculateBools()
 	if e != nil {
 		cmd.PrintErr(e)
@@ -69,10 +86,10 @@ func (r *runner) run(cmd *cobra.Command, args []string) {
 	r.Command = cmd
 	r.args = args
 
-	if r.bManual {
-		r.runManualJob()
-		return
-	}
+	// if r.bManual {
+	// 	r.runManualJob()
+	// 	return
+	// }
 
 	r.specs = r.makeSpecList()
 	if len(r.specs) == 0 {
@@ -110,15 +127,14 @@ func (r *runner) makeSpecList() []*dscore.Spec {
 }
 
 func (r *runner) handleFlags() error {
-	if r.bManual {
+	// if r.bOverrides {
+	//
+	// 	var overrides map[string]bool = make(map[string]bool, 1)
+	// }
+	if *r.fOptManualRun {
 
 	} else if r.bPartial { // partial/manual should not be runing at same time
 		r.processPartial()
-	}
-	if r.bOverrides {
-
-		var overrides map[string]bool = make(map[string]bool, 1)
-		_ = overrides
 	}
 
 	return nil
@@ -138,13 +154,8 @@ func (r *runner) processPartial() {
 // Errors on unusable combination of flags/args
 func (r *runner) calculateBools() error {
 	estr := ""
-	if manLen := len(*r.flagManualRun); manLen > 1 {
-		r.bManual = true
-	} else if manLen > 0 {
-		estr = "Manual run flag requires 2 arguments minimum\n"
-	}
 
-	if prtLen := len(*r.flagRunPartial); prtLen > 0 && !r.bManual {
+	if prtLen := len(*r.flagRunPartial); prtLen > 0 && !*r.fOptManualRun {
 		r.bPartial = true
 	} else if prtLen > 0 {
 		estr += "Partial flag and Manual flag are mutually exclusive\n"
@@ -164,9 +175,10 @@ func (r *runner) makeCopyJobs() {
 }
 
 func (r *runner) runManualJob() {
+
 	if len(r.args) == 2 {
-		Jp := dscore.JobManager()
-		job, e := Jp.SetupManual([]string{r.args[0]}, []string{r.args[1]})
+		jobmgr := dscore.JobManager()
+		job, e := jobmgr.SetupManual([]string{r.args[0]}, []string{r.args[1]})
 		_ = job
 		if e != nil {
 			r.PrintErr(e)
@@ -175,4 +187,5 @@ func (r *runner) runManualJob() {
 		}
 
 	}
+
 }
